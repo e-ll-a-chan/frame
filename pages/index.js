97d9850641c1f7760041d28050a75ac5343e40da
ella_chan_ <86010126+e-ll-a-chan@users.noreply.github.com>
2024-07-23 23:27:00 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,3 @@
-import Head from 'next/head'
-
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Warpcast Frame</title>
-        <meta property="og:title" content="Click me!" />
-        <meta property="og:image" content="https://raw.githubusercontent.com/e-ll-a-chan/frame/main/pic/0kcal.jpg" />
-        <meta name="fc:frame" content="vNext" />
-        <meta name="fc:frame:image" content="https://raw.githubusercontent.com/e-ll-a-chan/frame/main/pic/0kcal.jpg" />
-        <meta name="fc:frame:button:1" content="Click me" />
-        <meta name="fc:frame:post_url" content="https://0kcal-game.vercel.app/api/handle-click" />
-      </Head>
-      <main>
-        <h1>Welcome to my Warpcast Frame</h1>
-      </main>
-    </>
-  )
+  return <h1>Hello, World!</h1>
 }
