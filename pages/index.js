835492b293be7306a6bc82eb4a03ef887dbdc99c
ella_chan_ <86010126+e-ll-a-chan@users.noreply.github.com>
2024-07-23 22:53:48 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,11 @@ export default function Home() {
       <Head>
         <title>Warpcast Frame</title>
         <meta property="og:title" content="Click me!" />
-        <meta property="og:image" content="https://your-image-url.com/image.png" />
+        <meta property="og:image" content="https://github.com/e-ll-a-chan/frame/blob/main/pic/0kcal.jpg?raw=true" />
         <meta name="fc:frame" content="vNext" />
-        <meta name="fc:frame:image" content="https://your-image-url.com/image.png" />
+        <meta name="fc:frame:image" content="https://github.com/e-ll-a-chan/frame/blob/main/pic/0kcal.jpg?raw=true" />
         <meta name="fc:frame:button:1" content="Click me" />
-        <meta name="fc:frame:post_url" content="https://your-vercel-url.vercel.app/api/handle-click" />
+        <meta name="fc:frame:post_url" content="https://0kcal-game.vercel.app/api/handle-click" />
       </Head>
 
       <h1>{clicked ? 'おつかれ生' : 'Click the button!'}</h1>
