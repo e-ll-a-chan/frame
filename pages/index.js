2300e4926ab820a9ab6ea91062f2d8fa1318f2f6
ella_chan_ <86010126+e-ll-a-chan@users.noreply.github.com>
2024-07-23 23:07:08 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <meta name="fc:frame" content="vNext" />
         <meta name="fc:frame:image" content="https://raw.githubusercontent.com/e-ll-a-chan/frame/main/pic/0kcal.jpg" />
         <meta name="fc:frame:button:1" content="Click me" />
-        <meta name="fc:frame:post_url" content="https://0kcal-game.vercel.app/api/handle-click" />
+        <meta name="fc:frame:post_url" content="https://0kcal-game.vercel.app/" />
       </Head>
 
       <h1>{clicked ? 'おつかれ生' : 'Click the button!'}</h1>
