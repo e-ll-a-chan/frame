2c2b78a838d74b5a547b5678c51bc0064d1a3940
ella_chan_ <86010126+e-ll-a-chan@users.noreply.github.com>
2024-07-23 23:15:32 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
-
+import { useState } from 'react'
 
 export default function Home() {
   const [clicked, setClicked] = useState(false)
 
   return (
-    <div>
+    <html>
       <Head>
         <title>Warpcast Frame</title>
         <meta property="og:title" content="Click me!" />
@@ -13,10 +13,11 @@ export default function Home() {
         <meta name="fc:frame" content="vNext" />
         <meta name="fc:frame:image" content="https://picsum.photos/1200/630" />
         <meta name="fc:frame:button:1" content="Click me" />
-        <meta name="fc:frame:post_url" content="https://0kcal-game.vercel.app/" />
+        <meta name="fc:frame:post_url" content="https://0kcal-game.vercel.app/api/handle-click" />
       </Head>
-
-      <h1>{clicked ? 'おつかれ生' : 'Click the button!'}</h1>
-    </div>
+      <body>
+        <h1>{clicked ? 'おつかれ生' : 'Click the button!'}</h1>
+      </body>
+    </html>
   )
 }
