7d400ae135602c01e4663627f634c155ae39e1e8
ella_chan_ <86010126+e-ll-a-chan@users.noreply.github.com>
2024-07-23 22:54:52 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,9 @@ export default function Home() {
       <Head>
         <title>Warpcast Frame</title>
         <meta property="og:title" content="Click me!" />
-        <meta property="og:image" content="https://github.com/e-ll-a-chan/frame/blob/main/pic/0kcal.jpg?raw=true" />
+        <meta property="og:image" content="https://raw.githubusercontent.com/e-ll-a-chan/frame/main/pic/0kcal.jpg" />
         <meta name="fc:frame" content="vNext" />
-        <meta name="fc:frame:image" content="https://github.com/e-ll-a-chan/frame/blob/main/pic/0kcal.jpg?raw=true" />
+        <meta name="fc:frame:image" content="https://raw.githubusercontent.com/e-ll-a-chan/frame/main/pic/0kcal.jpg" />
         <meta name="fc:frame:button:1" content="Click me" />
         <meta name="fc:frame:post_url" content="https://0kcal-game.vercel.app/api/handle-click" />
       </Head>
